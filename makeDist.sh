922ecc250cdbd7f6a0f31a1e1236584e60ed325b
nilskp <nils@bulletproof.ninja>
2026-01-13 12:56:55 +0700

    fix: Don't compress zip file

--- makeDist.sh
+++ makeDist.sh
@@ -5,5 +5,5 @@ DIST_FILE=./.local/nostrrent-dist.zip
 ./mill jar
 ./mill zipRuntimeDeps
 cp out/zipRuntimeDeps.dest/deps.zip $DIST_FILE
-zip -j $DIST_FILE out/jar.dest/*.jar
+zip -j -r -0 $DIST_FILE out/jar.dest/*.jar
 echo "Distribution: $DIST_FILE"
