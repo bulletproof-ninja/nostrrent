5b556dc5f7be93ab587f5baa3657371a8cf49097
nilskp <nils@bulletproof.ninja>
2026-01-26 10:20:03 +0700

    fix: Don't serve `.torrent` files here

--- src/main/scala/nostrrent/web/jetty_scalatra/WebSeedServlet.scala
+++ src/main/scala/nostrrent/web/jetty_scalatra/WebSeedServlet.scala
@@ -1,6 +1,8 @@
 package nostrrent.web.jetty_scalatra
 
 import java.io.File
+import jakarta.servlet.http.{ HttpServletRequest, HttpServletResponse }
+import nostrrent.TorrentFileExt
 
 /**
   * Torrent content server (magnet link `ws` parameter).
@@ -9,3 +11,12 @@ import java.io.File
   */
 class WebSeedServlet(val urlPath: String, workDir: File)
 extends AbstractFileServer(workDir):
+
+  import HttpServletResponse.*
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit =
+    val filename = request.getPathInfo.drop(1)
+    if filename.endsWith(TorrentFileExt) && ! filename.contains("/") then
+      response.sendError(SC_NOT_FOUND)
+    else
+      super.doGet(request, response)
